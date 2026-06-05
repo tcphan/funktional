14853b526601642dfd0e6c2b70da9740563420c9
tcphan <tcphan1984@gmail.com>
2026-06-05 08:37:07 -0400

    Remove b_spline_math_notation() function

--- fda/basis.py
+++ fda/basis.py
@@ -178,6 +178,12 @@ class BSplineBasis(Basis):
         if key in self.cache:
             return self.cache[key]
 
+        # If index j is out of bounds, return 0.0
+        if j + p + 1 >= len(self.knots):
+            result = 0.0
+            self.cache[key] = result
+            return result
+
         # Base case: degree 0 (step functions)
         if p == 0:
             result = 1.0 if self.knots[j] <= x < self.knots[j+1] else 0.0
@@ -285,72 +291,6 @@ class BSplineBasis(Basis):
         # Display the plot
         plt.tight_layout()
         plt.show()
-        
-    def b_spline_math_notation(self, x, j):
-        """Generates the math notation for a B-spline basis function evaluation.
-        
-        Parameters
-        ----------
-        x : float
-            The point at which to evaluate the basis function.
-        j : int
-            The index of the basis function.
-            
-        Returns
-        -------
-        str
-            The math notation for the B-spline basis function evaluation.
-        """
-
-        def make_subscript(number):
-            """Convert a number to a subscript string."""
-            subscript_map = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
-            return str(number).translate(subscript_map)
-
-        # Convert to subscript
-        j_subscr = make_subscript(j)
-        p_subscr = make_subscript(self.degree)
-        j_plus_p_subscr = make_subscript(j+self.degree)
-        p_minus_1_subscr = make_subscript(self.degree-1)
-        j_plus_p_plus_1_subscr = make_subscript(j+self.degree+1)
-        j_plus_1_subscr = make_subscript(j+1)
-
-        # Line 1 math notation
-        part1 = f"({x} - t{j_subscr}/t{j_plus_p_subscr} - t{j_subscr}) * B{j_subscr},{p_minus_1_subscr}(x={x})"
-        part2 = f"(t{j_plus_p_plus_1_subscr} - {x}/t{j_plus_p_plus_1_subscr} - t{j_plus_1_subscr}) * B{j_plus_1_subscr},{p_minus_1_subscr}(x={x})"
-        math_notation_line1 = f"B{j_subscr},{p_subscr}(x={x}) = {part1} + {part2}"
-
-        # Line 2 math notation
-        part1 = f"({x} - {self.knots[j]}/{self.knots[j+self.degree]} - {self.knots[j]}) * B{j_subscr},{p_minus_1_subscr}(x={x})"
-        part2 = f"({self.knots[j+self.degree+1]} - {x}/{self.knots[j+self.degree+1]} - {self.knots[j+1]}) * B{j_plus_1_subscr},{p_minus_1_subscr}(x={x})"
-        math_notation_line2 = f"B{j_subscr},{p_subscr}(x={x}) = {part1} + {part2}"
-
-        # Line 3 math notation
-        weight1 = (x - self.knots[j])/(self.knots[j+self.degree] - self.knots[j])
-        weight2 = (self.knots[j+self.degree+1] - x)/(self.knots[j+self.degree+1] - self.knots[j+1])
-        part1 = f"{weight1:.4f} * B{j_subscr},{p_minus_1_subscr}(x={x})"
-        part2 = f"{weight2:.4f} * B{j_plus_1_subscr},{p_minus_1_subscr}(x={x})"
-        math_notation_line3 = f"B{j_subscr},{p_subscr}(x={x}) = {part1} + {part2}"
-
-        # Line 4 math notation
-        b_val1 = self._b_spline_value(x, j, self.degree)
-        b_val2 = self._b_spline_value(x, j+1, self.degree)
-        part1 = f"{weight1:.4f} * {b_val1}"
-        part2 = f"{weight2:.4f} * {b_val2}"
-        math_notation_line4 = f"B{j_subscr},{p_subscr}(x={x}) = {part1} + {part2}"
-
-        # Line 5 math notation
-        math_notation_line5 = f"B{j_subscr},{p_subscr}(x={x}) = {(weight1 * b_val1) + (weight2 * b_val2):.4f}"
-
-        # Combine all notation lines together
-        final_math_notation = "\n".join([
-            math_notation_line1,
-            math_notation_line2,
-            math_notation_line3,
-            math_notation_line4,
-            math_notation_line5
-        ])
-        return final_math_notation
 
 
 class FourierBasis(Basis):
